a4e0fce01bf9351559f64e47f3c91ca62b2e8188
AswinRam4433 <75494275+AswinRam4433@users.noreply.github.com>
2021-06-15 21:45:50 +0530

    Layout when scrolling is messed up
    When we scroll down the grey Topnav bar disappears
    It is not working well with fixed positioning
    It is only half visible when we give Fixed positioning

--- Induvidual Hotel JS.js
+++ Induvidual Hotel JS.js
@@ -5,9 +5,13 @@ function scrollFunction() {
     document.getElementById("header").style.fontSize = "25px";
     document.getElementById("headerImage").style.transform="scale(0.9)";
     document.getElementById("header").style.padding="18px 10px";
+    document.querySelector("#header").style.height="170px";
+    
   } else {
     document.getElementById("header").style.fontSize = "30px";
     document.getElementById("headerImage").style.transform="scale(1)";
-    document.getElementById("header").style.padding="30px 10px";
+    document.getElementById("header").style.padding="25px 10px";
+    document.querySelector("#header").style.height="200px";
+    
   }
 }
