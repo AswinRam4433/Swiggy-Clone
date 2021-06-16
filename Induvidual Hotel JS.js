0f0f622bff33b8d2802a989b2033a94b503033b1
AswinRam4433 <75494275+AswinRam4433@users.noreply.github.com>
2021-06-16 20:49:37 +0530

    Scrolling works like swiggy
    Scrolling is so far good
    Used negative margin instead of trying to use top
    Make the top ribbon smaller to enable better viewing

--- Induvidual Hotel JS.js
+++ Induvidual Hotel JS.js
@@ -6,12 +6,38 @@ function scrollFunction() {
     document.getElementById("headerImage").style.transform="scale(0.9)";
     document.getElementById("header").style.padding="18px 10px";
     document.querySelector("#header").style.height="170px";
+    /*window.getElementById("header").style.top="50px";*/
+    document.querySelector("#header").style.position="fixed";
+    document.querySelector("#header").style.marginTop="-50px";
+    console.log("IF has been triggered");
     
   } else {
     document.getElementById("header").style.fontSize = "30px";
     document.getElementById("headerImage").style.transform="scale(1)";
     document.getElementById("header").style.padding="25px 10px";
     document.querySelector("#header").style.height="200px";
+    /*window.getElementById("header").style.top="0px";*/
+    document.querySelector("#header").style.position="fixed";
+    document.querySelector("#header").style.marginTop="0px";
+    console.log("ELSE has been triggered");
     
   }
 }
+
+
+
+// document.querySelectorAll("#myTopnav a" ).addEventListener("click", clickFunction);
+
+// function clickFunction() {
+//   console.log("Top Nav Button has been clicked");
+// }
+function clicked(){
+  console.log("This button has been clicked");
+}
+const allAinTopNav = document.querySelectorAll("myTopnav a");
+
+ for (let i = 0; i < allAinTopNav.length; i++) {
+     allAinTopNav[i].addEventListener("click", function() {
+      console.log("Button Has Been clicked")
+     });
+ }
